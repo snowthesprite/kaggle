96896a30762dd8d0dd1507fb5bc43b4771c88b7a
snowthesprite <mpapaya9@gmail.com>
2021-03-29 01:18:11 +0000

    Made the current analysis of the titanic data

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+import sys
 
 df = pd.read_csv('/home/runner/kaggle/dataset.csv')
 
@@ -37,18 +39,102 @@ df['Parch>0'] = df['Parch'].apply(id_greater_than_zero)
 del df['Parch']
 
 
-'''
+df['Cabin'] = df['Cabin'].fillna('None')
 def get_cabin_type (cabin) :
-    if np.isnan(cabin) :
-        return ''
-    else :
+    if cabin != 'None' :
         return cabin[0]
+    else :
+        return cabin
 
-print(df['Cabin'].apply(get_cabin_type))
-#'''
+df['CabinType'] = df['Cabin'].apply(get_cabin_type)
+
+for cabin_type in df['CabinType'].unique() :
+    dummy_name = 'CabinType=' + cabin_type
+    dummy_vals = df['CabinType'].apply(lambda entry : int(entry == cabin_type))
+    df[dummy_name] = dummy_vals
+
+del df['CabinType']
+
+
+df['Embarked'] = df['Embarked'].fillna('None')
+
+for port in df['Embarked'].unique() :
+    dummy_name = 'Embarked=' + port
+    dummy_vals = df['Embarked'].apply(lambda entry : int(entry == port))
+    df[dummy_name] = dummy_vals
+
 
 del df['Cabin']
 del df['Embarked']
 
-train_df = df[:500]
-test_df = [500:]
+
+#''''
+used_features = [
+    'Sex',
+    'Pclass',
+    'Fare',
+    'Age',
+    'SibSp', 'SibSp>0',
+    'Parch>0',
+    'Embarked=C', 'Embarked=None', 'Embarked=Q', 'Embarked=S', 
+    'CabinType=A', 'CabinType=B', 'CabinType=C', 'CabinType=D', 'CabinType=E', 'CabinType=F', 'CabinType=G', 'CabinType=None', 'CabinType=T']
+
+cols_needed = ['Survived'] + used_features
+df = df[cols_needed]
+#'''
+
+
+
+df_train = df[:500]
+df_test = df[500:]
+
+arr_train = np.array(df_train)
+arr_test = np.array(df_test)
+
+y_train = arr_train[:,0]
+y_test = arr_test[:,0]
+
+x_train = arr_train[:,1:]
+x_test = arr_test[:,1:]
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+coeff_dict = {'Constant' : regressor.intercept_}
+feature_cols = df_train.columns[1:]
+feature_coeffs = regressor.coef_
+
+for i in range(len(feature_cols)) :
+    col = feature_cols[i]
+    coeff = feature_coeffs[i]
+    coeff_dict[col] = coeff
+
+y_test_predictions = regressor.predict(x_test)
+y_train_predictions = regressor.predict(x_train)
+
+def from_regress_out_to_survival_predict (output) :
+    if output < 0.5 :
+        return 0 
+    else :
+        return 1
+
+y_test_predictions = [from_regress_out_to_survival_predict(output) for output in y_test_predictions]
+y_train_predictions = [from_regress_out_to_survival_predict(output) for output in y_train_predictions]
+
+def find_accuracy (predict, actual) :
+    num_correct = 0
+    num_incorrect = 0
+    for i in range(len(predict)) :
+        if predict[i] == actual[i] :
+            num_correct += 1
+        else :
+            num_incorrect+=1 
+    return num_correct/(num_correct + num_incorrect)
+
+
+
+print("\n",'features:', used_features)
+print('train:', find_accuracy(y_train_predictions, y_train))
+print('test:', find_accuracy(y_test_predictions, y_test), "\n")
+
+print(coeff_dict)
